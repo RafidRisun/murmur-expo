ae878abc25aba33f722597652ad409a3423f4209
RafidRisun <rafidhassanrisun@gmail.com>
2025-12-22 01:55:55 +0600

    End of the night

--- src/app/(tabs)/home/index.tsx
+++ src/app/(tabs)/home/index.tsx
@@ -2,7 +2,13 @@ import { useAuth } from "@/src/context/authContext";
 import { createMurmur } from "@/src/services/murmurmService";
 import { Button, Input } from "@ui-kitten/components";
 import React, { useEffect, useState } from "react";
-import { ActivityIndicator, Alert, View } from "react-native";
+import {
+  ActivityIndicator,
+  Alert,
+  Keyboard,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 import tw from "twrnc";
 
 export default function Index() {
@@ -39,16 +45,18 @@ export default function Index() {
   };
 
   return (
-    <View style={tw`flex-1 justify-start items-center bg-black p-4`}>
-      <Input
-        style={tw`w-full`}
-        placeholder="Any new Murmur?"
-        value={newMurmur}
-        onChangeText={setNewMurmur}
-      />
-      <Button style={tw`mt-4 w-full`} onPress={handlePost}>
-        Post Murmur
-      </Button>
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} style={{ flex: 1 }}>
+      <View style={tw`flex-1 justify-start items-center bg-black p-4`}>
+        <Input
+          style={tw`w-full`}
+          placeholder="Any new Murmur?"
+          value={newMurmur}
+          onChangeText={setNewMurmur}
+        />
+        <Button style={tw`mt-4 w-full`} onPress={handlePost}>
+          Post Murmur
+        </Button>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
